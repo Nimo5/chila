d110a197595f2d3dbd71330af5a0274acb67ed85
Nimo5 <92031790+Nimo5@users.noreply.github.com>
2024-11-13 19:26:00 +0100

    Add files via upload

---
+++ chila.js
@@ -0,0 +1,57 @@
+function test1(){
+    if(document.getElementById('textaimen').value=='manini'){
+
+        document.getElementById('text_aimen').innerText='z3iiiiiiim';
+    }else{
+        document.getElementById('text_aimen').innerText='mb3rrrrrrrrrrrr ';
+    }
+}  
+function test2(){
+    if(document.getElementById('textmaroun').value=='gay'){
+
+        document.getElementById('text_maroun').innerText='chikor';
+    }else{
+        document.getElementById('text_maroun').innerText='mb3rrrrrrrrrrrr';
+    }
+} 
+function test3(){
+    if(document.getElementById('textsohaib').value=='9baily'){
+
+        document.getElementById('text_sohaib').innerText='s7aaaaaaaaaaaaa';
+    }else{
+        document.getElementById('text_sohaib').innerText='mb3rrrrrrrrrrrr ';
+    }
+} 
+function test4(){
+    if(document.getElementById('textamin').value=='toba'){
+
+        document.getElementById('text_amin').innerText='9waaaa';
+    }else{
+        document.getElementById('text_amin').innerText='mb3rrrrrrrrrrrr ';
+    }
+} 
+function test5(){
+    if(document.getElementById('textrida').value=='imam'){
+
+        document.getElementById('text_rida').innerText='n7bk';
+    }else{
+        document.getElementById('text_rida').innerText='mb3rrrrrrrrrrrr ';
+    }
+}
+function test6(){
+    if(document.getElementById('texthkim').value=='hakim'){
+
+        document.getElementById('text_hkim').innerText='kifach 3rft ???';
+    }else{
+        document.getElementById('text_hkim').innerText='mb3rrrrrrrrrrrr ';
+    }
+}
+function testf(){ 
+if(document.getElementById('textaimen').value=='manini' && document.getElementById('textmaroun').value=='gay' && document.getElementById('textsohaib').value=='9baily' && document.getElementById('textamin').value=='toba' && document.getElementById('textrida').value=='imam' && document.getElementById('texthkim').value=='hakim'){
+ document.getElementById('finaltext').innerText='yamo';
+}else{
+    document.getElementById('finaltext').innerText='rak galt fi 3fsa khmm mli7 ';
+}
+}
+
+
